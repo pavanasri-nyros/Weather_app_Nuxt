ff8e21089bfd4aab566d5a38e10c265b43d59460
Pavana sri vinaya <pavanasri.nyros@gmail.com>
2020-12-01 09:29:49 +0530

    second

---
+++ layouts/default.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-app dark>
+    <v-app-bar fixed app>
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+      <v-toolbar-items class="hidde-sm-and-down">
+        <v-btn text>
+          <nuxt-link to="/weather-app"> Weather app </nuxt-link>
+        </v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Weather',
+    }
+  },
+}
+</script>
+
+<style scoped>
+a {
+  text-decoration: none;
+  color: rgb(251, 247, 247);
+}
+</style>
---
+++ pages/weather-app.vue
@@ -0,0 +1,77 @@
+<template>
+  <v-container>
+    <v-flex>
+      <v-card color="dark-grey darken-2" dark>
+        <v-card-text>
+          <v-layout class="justify-center">
+            <v-flex v-if="weather.weather" class="text-xs-center">
+              <h4>Temperature</h4>
+              <h1>{{ weather.name }}</h1>
+              <img :src="icon" alt="weather icon" />
+              <p>
+                <span class="display-1">{{ temp() }} &deg;C</span>
+                <span class="caption">{{
+                  weather.weather[0].description
+                }}</span>
+              </p>
+            </v-flex>
+          </v-layout>
+        </v-card-text>
+      </v-card>
+    </v-flex>
+    <v-flex xs12 class="mt-4">
+      <v-form @submit.prevent="getWeatherInfo">
+        <v-textarea
+          v-model="city"
+          label="Enter city name"
+          outlined
+          rows="1"
+          row-height="15"
+          solo
+        ></v-textarea>
+        <v-btn @click="getWeatherInfo">Submit</v-btn>
+      </v-form>
+    </v-flex>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      city: 'Kakinada',
+      weather: {},
+    }
+  },
+  computed: {
+    icon() {
+      return this.weather.weather
+        ? `https://openweathermap.org/img/w/${this.weather.weather[0].icon}.png`
+        : ''
+    },
+  },
+  created() {
+    this.getWeatherInfo()
+  },
+  methods: {
+    getWeatherInfo() {
+      this.$axios
+        .$get(
+          `http://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${process.env.weatherAppId}`
+        )
+        // eslint-disable-next-line no-console
+        .then((res) => (this.weather = res))
+    },
+    temp() {
+      return this.weather.main ? Math.round(this.weather.main.temp - 273) : ''
+    },
+  },
+  head() {
+    return {
+      title: 'Weather app',
+    }
+  },
+}
+</script>
+
+<style lang="stylus" scoped></style>
