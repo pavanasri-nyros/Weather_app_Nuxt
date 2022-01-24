4c722e361db69192ff85d35e9c66bd14da75f5b2
pavana sri <36202394+pavanasrivinaya@users.noreply.github.com>
2022-01-24 07:42:55 +0000

    Create about.vue

---
+++ pages/about.vue
@@ -0,0 +1,29 @@
+<!--
+     Author:    Build Rise Shine with Nyros (BRS) 
+     Created:   11.05.2022 
+     Library / Component: About page
+     Description: Page displays about the website working
+     (c) Copyright by BRS with Nyros. 
+-->
+<template>
+  <v-container class="about">
+    <v-card class="mx-auto mt-5" max-width="500" outlined>
+      <v-list-item three-line>
+        <span>
+          👉🏿 API: Weather API
+
+          <br />
+          👉🏿
+          <a href="https://openweathermap.org/api">
+            API link: https://openweathermap.org/api</a
+          >
+          <br />
+          <h5>
+            👉🏿Purpose : API is used to access current weather data for any
+            location
+          </h5>
+        </span>
+      </v-list-item>
+    </v-card>
+  </v-container>
+</template>
