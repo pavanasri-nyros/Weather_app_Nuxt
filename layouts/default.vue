6de71e8dd8f34158873074b21032e1f3c2e24144
pavana sri <36202394+pavanasrivinaya@users.noreply.github.com>
2022-01-24 07:42:13 +0000

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,21 @@
+<!--
+     Author:    Build Rise Shine with Nyros (BRS) 
+     Created:   11.05.2022 
+     Library / Component: Default layout
+     Description: Displays  the layout for the app which is like a component 
+     (c) Copyright by BRS with Nyros. 
+-->
 <template>
-  <v-app dark>
+  <v-app>
     <v-app-bar fixed app>
       <v-toolbar-title v-text="title" />
       <v-spacer />
       <v-toolbar-items class="hidde-sm-and-down">
         <v-btn text>
-          <nuxt-link to="/weather-app"> Weather app </nuxt-link>
+          <nuxt-link to="/weather-app"> Home </nuxt-link>
+        </v-btn>
+        <v-btn text>
+          <nuxt-link to="/about"> ABOUT API </nuxt-link>
         </v-btn>
       </v-toolbar-items>
     </v-app-bar>
@@ -31,5 +41,9 @@ export default {
 a {
   text-decoration: none;
   color: rgb(251, 247, 247);
+}
+.theme--dark.v-application {
+  background: white !important;
+  color: #ffffff;
 }
 </style>
