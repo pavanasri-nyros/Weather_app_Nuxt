558d7a2639fd3b8ce9badd5ab2ff3ee764cfeaa6
pavana sri <36202394+pavanasrivinaya@users.noreply.github.com>
2022-01-24 07:43:26 +0000

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,10 @@
+<!--
+     Author:    Build Rise Shine with Nyros (BRS) 
+     Created:   11.05.2022 
+     Library / Component: Home Page
+     Description: Page displays Home for the app
+     (c) Copyright by BRS with Nyros. 
+-->
 <template>
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
